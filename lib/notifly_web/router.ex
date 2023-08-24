a3201cfdb52931a67c6abb59e471420099e19e74
jexi-The Linux <vnjeru85@gmail.com>
2023-08-24 08:37:02 +0300

    render auth_root layout for authenticated pages

--- lib/notifly_web/router.ex
+++ lib/notifly_web/router.ex
@@ -65,6 +65,7 @@ defmodule NotiflyWeb.Router do
     pipe_through [:browser, :require_authenticated_user]
 
     live_session :require_authenticated_user,
+      root_layout: {NotiflyWeb.Layouts, :auth_root},
       on_mount: [{NotiflyWeb.UserAuth, :ensure_authenticated}] do
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
