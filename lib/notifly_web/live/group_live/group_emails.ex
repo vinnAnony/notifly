ef8511c7db9fb04c468f72c39f1d25f2fbcc0eed
jexi-The Linux <vnjeru85@gmail.com>
2023-08-29 11:53:40 +0300

    modify page_title for group emails

--- lib/notifly_web/live/group_live/group_emails.ex
+++ lib/notifly_web/live/group_live/group_emails.ex
@@ -1,5 +1,16 @@
 defmodule NotiflyWeb.GroupLive.GroupEmails do
+  alias Notifly.Groups.Group
+  alias Notifly.Repo
   use NotiflyWeb, :live_view
 
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
 
+  @impl true
+  def handle_params(%{"id" => id}, _url, socket) do
+    group = Repo.get(Group, id)
+    {:noreply, socket|> assign(:page_title, "#{group.name} Emails")}
+  end
 end
