5ce78fd912e39155d17080cbaf9be14991a7ff2c
jexi-The Linux <vnjeru85@gmail.com>
2023-08-31 16:15:45 +0300

    fix: update group email counts after failed email is retried

--- lib/notifly/workers/email_worker.ex
+++ lib/notifly/workers/email_worker.ex
@@ -36,6 +36,7 @@ defmodule Notifly.Workers.EmailWorker do
       {:sent, _message} ->
         # Update email status
         email = Repo.get(Email, email_id)
+
         Emails.update_email(email, %{status: :sent})
         # update sent group emails count
         if email.ge_id do
@@ -44,12 +45,23 @@ defmodule Notifly.Workers.EmailWorker do
           new_pending_count = group_email.pending_emails - 1
 
           # Compare no. of email statuses to determine group email status
-          if new_success_count == group_email.no_of_emails do
-            new_pending_count = 0
-            GroupEmails.update_group_email(group_email, %{success_emails: new_success_count, pending_emails: new_pending_count, status: :sent})
+          if is_retry_failed_email?(email) do
+            new_failed_count = group_email.failed_emails - 1
+            if new_success_count == group_email.no_of_emails do
+              new_failed_count = 0
+              GroupEmails.update_group_email(group_email, %{success_emails: new_success_count, failed_emails: new_failed_count, status: :sent})
+            else
+              GroupEmails.update_group_email(group_email, %{success_emails: new_success_count, failed_emails: new_failed_count})
+            end
           else
-            GroupEmails.update_group_email(group_email, %{success_emails: new_success_count, pending_emails: new_pending_count})
+            if new_success_count == group_email.no_of_emails do
+              new_pending_count = 0
+              GroupEmails.update_group_email(group_email, %{success_emails: new_success_count, pending_emails: new_pending_count, status: :sent})
+            else
+              GroupEmails.update_group_email(group_email, %{success_emails: new_success_count, pending_emails: new_pending_count})
+            end
           end
+
         end
 
         Endpoint.broadcast(channel, "email:sent", %{email_id: email_id, status: :sent, progress: 100})
@@ -58,6 +70,15 @@ defmodule Notifly.Workers.EmailWorker do
       10_000 ->
         Endpoint.broadcast(channel, "email:failed", %{email_id: email_id, status: :failed ,progress: 0})
         raise RuntimeError, "no progress after 10s"
+    end
+  end
+
+  defp is_retry_failed_email?(email) do
+    IO.inspect(email)
+    if email.status == :failed do
+      true
+    else
+      false
     end
   end
 end
